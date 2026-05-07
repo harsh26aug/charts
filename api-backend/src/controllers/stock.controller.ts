39894e47cc9c3248c35fda2f613306d1eff1e105
Harshad Patel <harsh26aug@gmail.com>
2026-05-07 16:22:42 +0530

    Feat - API created to get sensex data

--- api-backend/src/controllers/stock.controller.ts
+++ api-backend/src/controllers/stock.controller.ts
@@ -6,12 +6,38 @@ import { getValidatedQuery } from '@middleware/validate.middleware';
 export class StockController {
     private stockService = new StockService();
 
-    getNiftyStockHistory = async (_req: Request, res: Response): Promise<void> => {
-        const result = await this.stockService.getNiftyStockHistory(
-            getValidatedQuery<StockPaginationQueryDto>(res),
-        );
+    getNiftyStockHistory = async (
+      _req: Request,
+      res: Response,
+    ): Promise<void> => {
+      const result = await this.stockService.getNiftyStockHistory(
+        getValidatedQuery<StockPaginationQueryDto>(res),
+      );
 
-        res.status(200).json({ success: true, data: result.items, pagination: result.pagination });
+      res
+        .status(200)
+        .json({
+          success: true,
+          data: result.items,
+          pagination: result.pagination,
+        });
+    };
+
+    getSensexStockHistory = async (
+      _req: Request,
+      res: Response,
+    ): Promise<void> => {
+      const result = await this.stockService.getSensexStockHistory(
+        getValidatedQuery<StockPaginationQueryDto>(res),
+      );
+
+      res
+        .status(200)
+        .json({
+          success: true,
+          data: result.items,
+          pagination: result.pagination,
+        });
     };
 }
 
